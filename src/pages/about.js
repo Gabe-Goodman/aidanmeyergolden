3f383b2fb29043faa16db3db184c602c1b1d6318
Gabe-Goodman <goodman.g@husky.neu.edu>
2024-11-11 19:48:08 -0500

    About edit

--- src/pages/about.js
+++ src/pages/about.js
@@ -21,21 +21,62 @@ const about = ({ data }) => {
           Aidan is an author and songwriter. His idiosyncratic poetic work draws on themes from
           linguistics, philosophy, world poetry and religions. Musically, influences are varied,
           ranging from Hindustani classical, math rock, jazz, and an adolescence spent entrenched in
-          Boston's DIY music scene. Current Musical Projects: Katsu Bandleader of ongoing project
-          with various lineups including: Nose Always In Blossoms (2019) BALANCE (2023) piano/tenor
-          sax duo EP (recorded 2022, pending release). Past Musical Collaborations: Modern Painters
+          Boston's DIY music scene.
+          <br />
+          <br />
+          Current Musical Projects: Katsu Bandleader of ongoing project with various lineups
+          including:
+          <br />
+          Nose Always In Blossoms (2019)
+          <br />
+          BALANCE (2023)
+          <br />
+          piano/tenor sax duo EP (recorded 2022, pending release).
+          <br />
+          <br />
+          Past Musical Collaborations:
+          <br />
+          Modern Painters
+          <br />
           City Folk, 2019, arranged and performed Electric Bass on full album of original
-          compositions Shit Whitman performed electric bass on two yet unreleased studio albums, as
-          well as some lead guitar Grants: Mass MoCA - Assets for Artists (A4A) - 2024 Capacity
-          Building Grant program Residency: Floating Tower Immigrant & Refugee Artists Retreat at
-          Chase Hill - 2020-2022 Participated in all aspects of managing and developing retreat
-          space on both volunteer and employee basis, including participation as musician and
-          performer in the June 2023 staged theater production Shadows of Forgotten Ancestors,
-          directed by Matti Kovler Other Artistic Work: Author Novella in progress “Between
-          Branches” Poetry manuscript “Angles on Emptiness”, seeking pubisher Past Publications
-          Northeastern University Spectrum Literary Magazine Contributor, 2016, 2017; Poetry The
-          Foundationalist, Volume 1, Spring 2018; Short Story: Dead Dog Teaching: Private music
-          lessons in guitar, ear training, piano, and music theory
+          compositions
+          <br />
+          Shit Whitman
+          <br />
+          performed electric bass on two yet unreleased studio albums, as well as some lead guitar
+          <br />
+          <br />
+          Grants: Mass MoCA - Assets for Artists (A4A) - 2024 Capacity Building Grant program
+          Residency: Floating Tower Immigrant & Refugee Artists Retreat at Chase Hill - 2020-2022
+          <br />
+          Participated in all aspects of managing and developing retreat space on both volunteer and
+          employee basis, including participation as musician and performer in the June 2023 staged
+          theater production Shadows of Forgotten Ancestors, directed by Matti Kovler
+          <br />
+          <br />
+          Other Artistic Work:
+          <br />
+          Author
+          <br />
+          Novella in progress “Between Branches”
+          <br />
+          Poetry manuscript “Angles on Emptiness”, seeking pubisher
+          <br />
+          <br />
+          Past Publications
+          <br />
+          Northeastern University Spectrum Literary Magazine
+          <br />
+          Contributor, 2016, 2017; Poetry
+          <br />
+          <br />
+          The Foundationalist, Volume 1, Spring 2018; Short Story: Dead Dog
+          <br />
+          <br />
+          Teaching:
+          <br />
+          <br />
+          Private music lessons in guitar, ear training, piano, and music theory
         </H1>
       </Content>
     </Container>
